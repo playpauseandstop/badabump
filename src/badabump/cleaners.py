d10288b71ea4a8a6effe9fd325ae13ec6e44d415
Igor Davydenko <iam@igordavydenko.com>
2020-10-24 01:53:20 +0200

    feat: Properly clean commit body on preparing release tag (#8)
    As well as use Badabump App for creating release PR to fix issue with
    not running expected checks, while creating PR via `github-actions` bot.
    
    Also create release tag by `badabump-release-bot` user, not via
    `github-actions` user.

---
+++ src/badabump/cleaners.py
@@ -0,0 +1,25 @@
+import re
+from typing import List
+
+
+CO_AUTHORED_BY = "Co-authored-by: "
+COMMIT_SUBJECT_WITH_PR_RE = re.compile(r"^(?P<subject>.+) \(\#\d+\)$")
+
+
+def clean_body(body: List[str]) -> str:
+    cleaned = "\n".join(
+        item for item in body if not item.startswith(CO_AUTHORED_BY)
+    )
+    if cleaned[-1:] == "\n":
+        return cleaned
+    return f"{cleaned}\n"
+
+
+def clean_commit_subject(value: str) -> str:
+    return COMMIT_SUBJECT_WITH_PR_RE.sub(r"\1", value)
+
+
+def clean_tag_ref(value: str) -> str:
+    if value[:10] == "refs/tags/":
+        return value[10:]
+    return value

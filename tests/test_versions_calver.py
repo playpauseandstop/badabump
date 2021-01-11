6cb1c0a7941c8f1ec12c51714cad71c081eb1fcb
Igor Davydenko <iam@igordavydenko.com>
2021-01-11 19:10:36 +0100

    fix(calver): Properly update CalVer scheme between years (#45)
    If project uses CalVer and it previous version was `20.6.0`, running
    `badabump` in 2021 result in `21.7.0` version, which is wrong.
    
    This commit fixes logic behind CalVer updates and always reset minor,
    micro to `1.0` if year, month, week, or day present in schema and is
    changed.
    
    Fixes: #44

--- tests/test_versions_calver.py
+++ tests/test_versions_calver.py
@@ -1,7 +1,9 @@
 import datetime
 
 import pytest
+import time_machine
 
+from badabump.configs import UpdateConfig
 from badabump.versions.calver import CalVer, SHORT_YEAR_START
 
 UTCNOW = datetime.datetime.utcnow()
@@ -22,3 +24,148 @@ DAY = UTCNOW.day
 )
 def test_calver_initial(schema, expected):
     assert CalVer.initial(schema=schema).format() == expected
+
+
+@pytest.mark.parametrize(
+    "schema, version, update_kwargs, expected",
+    (
+        (
+            "YYYY.MINOR.MICRO",
+            "2018.1.0",
+            {
+                "is_breaking_change": True,
+                "is_minor_change": False,
+                "is_micro_change": False,
+                "is_pre_release": False,
+            },
+            "2021.1.0",
+        ),
+        (
+            "YYYY.MINOR.MICRO",
+            "2018.1.0",
+            {
+                "is_breaking_change": True,
+                "is_minor_change": False,
+                "is_micro_change": False,
+                "is_pre_release": True,
+            },
+            "2021.1.0",
+        ),
+        (
+            "YY.0M_MINOR.MICRO",
+            "18.01_1.0",
+            {
+                "is_breaking_change": True,
+                "is_minor_change": False,
+                "is_micro_change": False,
+                "is_pre_release": False,
+            },
+            "21.01_1.0",
+        ),
+        (
+            "YY.0M_MINOR.MICRO",
+            "18.01_1.0",
+            {
+                "is_breaking_change": True,
+                "is_minor_change": False,
+                "is_micro_change": False,
+                "is_pre_release": True,
+            },
+            "21.01_1.0",
+        ),
+        (
+            "YYYY.MINOR",
+            "2019.3",
+            {
+                "is_breaking_change": False,
+                "is_minor_change": True,
+                "is_micro_change": False,
+                "is_pre_release": False,
+            },
+            "2021.1",
+        ),
+        (
+            "YYYY.MINOR",
+            "2019.3",
+            {
+                "is_breaking_change": False,
+                "is_minor_change": True,
+                "is_micro_change": False,
+                "is_pre_release": True,
+            },
+            "2021.1",
+        ),
+        (
+            "YYYY.MM.DD",
+            "2019.3.1",
+            {
+                "is_breaking_change": False,
+                "is_minor_change": True,
+                "is_micro_change": False,
+                "is_pre_release": False,
+            },
+            "2021.1.11",
+        ),
+        (
+            "YYYY.MM.DD",
+            "2019.3.1",
+            {
+                "is_breaking_change": False,
+                "is_minor_change": True,
+                "is_micro_change": False,
+                "is_pre_release": True,
+            },
+            "2021.1.11",
+        ),
+        (
+            "YY_MINOR.MICRO",
+            "20_6.8",
+            {
+                "is_breaking_change": False,
+                "is_minor_change": False,
+                "is_micro_change": True,
+                "is_pre_release": False,
+            },
+            "21_1.0",
+        ),
+        (
+            "YY_MINOR.MICRO",
+            "20_6.8",
+            {
+                "is_breaking_change": False,
+                "is_minor_change": False,
+                "is_micro_change": True,
+                "is_pre_release": True,
+            },
+            "21_1.0",
+        ),
+        (
+            "YYYY.MINOR.MICRO",
+            "2020.6.8",
+            {
+                "is_breaking_change": False,
+                "is_minor_change": False,
+                "is_micro_change": True,
+                "is_pre_release": False,
+            },
+            "2021.1.0",
+        ),
+        (
+            "YYYY.MINOR.MICRO",
+            "2020.6.8",
+            {
+                "is_breaking_change": False,
+                "is_minor_change": False,
+                "is_micro_change": True,
+                "is_pre_release": True,
+            },
+            "2021.1.0",
+        ),
+    ),
+)
+def test_calver_update_year_change(schema, version, update_kwargs, expected):
+    update_config = UpdateConfig(**update_kwargs)
+    current = CalVer.parse(version, schema=schema)
+
+    with time_machine.travel("2021-01-11T00:00:00+00:00"):
+        assert current.update(update_config).format() == expected
